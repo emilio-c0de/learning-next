a2a42f24d0549ff2f9fe7b314973bf46c844d187
Emilio Senguana <sengua2000@outlook.com>
2023-08-26 17:19:02 -0500

    suspense

--- lesson01/app/post/[id]/page.tsx
+++ lesson01/app/post/[id]/page.tsx
@@ -1,6 +1,7 @@
  
 import { Post } from '@/models/Post.model';
-import React from 'react'
+import React, { Suspense } from 'react'
+import Posts from './../page'
 // app/posts/page.ts
 
 type Props = {
@@ -22,6 +23,10 @@ export default async function Page(props: Props) {
     <div>
         <h1>{data.title}</h1>
         <p>{data.body}</p>
+        <hr />
+        <Suspense fallback={<><h1>Cargando publicaciones...</h1></>}>
+            <Posts/>
+        </Suspense>
     </div>
   )
 }
